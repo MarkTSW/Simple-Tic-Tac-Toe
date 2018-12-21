45a0333dc0cb4822781a379059d4d19e31d4f031
R1vALHimself <32874703+R1vALHimself@users.noreply.github.com>
2018-12-21 21:27:10 +0200

    Full Game
    Full Game

---
+++ wf_tictactoe/wf_tictactoe/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace wf_tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(22, 6, 0, 6);
+            this.menuStrip1.Size = new System.Drawing.Size(377, 31);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 19);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 19);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.DarkRed;
+            this.A1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.A1.Location = new System.Drawing.Point(10, 52);
+            this.A1.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(116, 120);
+            this.A1.TabIndex = 1;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.DarkRed;
+            this.A2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.A2.Location = new System.Drawing.Point(135, 52);
+            this.A2.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(118, 120);
+            this.A2.TabIndex = 2;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.DarkRed;
+            this.A3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.A3.Location = new System.Drawing.Point(258, 52);
+            this.A3.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(109, 120);
+            this.A3.TabIndex = 3;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.DarkRed;
+            this.B1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.B1.Location = new System.Drawing.Point(10, 180);
+            this.B1.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(116, 106);
+            this.B1.TabIndex = 4;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.DarkRed;
+            this.B2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.B2.Location = new System.Drawing.Point(135, 180);
+            this.B2.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(118, 106);
+            this.B2.TabIndex = 5;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.DarkRed;
+            this.B3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.B3.Location = new System.Drawing.Point(258, 180);
+            this.B3.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(109, 106);
+            this.B3.TabIndex = 6;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.DarkRed;
+            this.C1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.C1.Location = new System.Drawing.Point(10, 297);
+            this.C1.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(116, 112);
+            this.C1.TabIndex = 7;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.DarkRed;
+            this.C2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.C2.Location = new System.Drawing.Point(135, 297);
+            this.C2.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(118, 112);
+            this.C2.TabIndex = 8;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.DarkRed;
+            this.C3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.C3.Location = new System.Drawing.Point(258, 297);
+            this.C3.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(109, 112);
+            this.C3.TabIndex = 9;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(377, 428);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+    }
+}
+
